20f4f110b98543a90359442344d41f6960d549cd
Hannah Phillips <135017357+hannahbradee@users.noreply.github.com>
2023-09-28 11:06:20 +1300

    Update sketch.js
    deleting code that doesnt work

--- sketch.js
+++ sketch.js
@@ -3,9 +3,9 @@ let maskImg=null;
 let renderCounter=0;
 
 // change these three lines as appropiate
-let sourceFile = "Input_3.jpg";
-let maskFile   = "Mask_3.png";
-let outputFile = "output_3.png";
+let sourceFile = "Input_1.jpg";
+let maskFile   = "Mask_1.png";
+let outputFile = "output_1.png";
 
 function preload() {
   sourceImg = loadImage(sourceFile);
@@ -25,32 +25,27 @@ function setup () {
 
 
 function draw () {
-  for(let i=0;i<20000;i++) {
+  for(let i=0;i<15000;i++) {
     let x = floor(random(sourceImg.width));
     let y = floor(random(sourceImg.height));
     let pix = sourceImg.get(x, y);
     let mask = maskImg.get(x, y);
     fill(pix);
     if(mask[0] > 128) {
-      let pointSize = 5;
-      rect(x, y, pointSize, pointSize);
+      let pointSizeW = 2.5;
+      let pointSizeL =15;
+      rect(x, y, pointSizeW, pointSizeL);
     }
     else {
-      let pointSize = 20;
-      ellipse(x, y, pointSize, pointSize); 
+      let pointSizeW = 2;
+      let pointSizeL =50;
+      rect(x, y, pointSizeL, pointSizeW); 
           
     }
-    //Squares(10,10,50);
     
   }
-  //function Squares(x,y,size){
-  
-   // for(let ii=0; ii<50; ii+10){
-     // fill(0,0,0,50);
-     // rect(x+ii, y+ii, size, size);
-   // }
- // }
 
+  
   }
   renderCounter = renderCounter + 1;
   if(renderCounter > 10) {
