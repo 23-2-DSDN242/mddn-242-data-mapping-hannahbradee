7c2034fdee6bef0f2551662aaf03dc3c168d7377
Hannah Phillips <135017357+hannahbradee@users.noreply.github.com>
2023-10-11 20:08:56 +1300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,15 +5,17 @@ let maskImg=null;
 // change these three lines as appropiate
 
 let sourceFile = "input_3.jpg";
-let maskFile   = "mask_3.png";
+let maskFile   = "Mask_3.png";
+let textureFile = "TexMask_3.png";
 let outputFile = "output_3.png";
 
-l
+
 
 function preload() {
 
   sourceImg = loadImage(sourceFile);
   maskImg = loadImage(maskFile);
+  textureImg= loadImage(textureFile)
   colorMode(HSB);
 }
 
@@ -26,6 +28,11 @@ function setup () {
   background(255, 255, 255);
   sourceImg.loadPixels();
   maskImg.loadPixels();
+  textureImg.loadPixels();
+
+  
+
+ 
 }
 
 
@@ -36,26 +43,13 @@ let OFFSET = 20;
 
 let renderCounter=0;
 
+
+
+
 function draw () {
-  //for(let i=0;i<100;i++) {
-   // let x = floor(random(sourceImg.width));
-   // let y = floor(random(sourceImg.height));
-    //let pix = sourceImg.get(x, y);
-   // let mask = maskImg.get(x, y);
-   // fill(pix);
-   // if(mask[0] > 128) {
-   //   let pointSizeW = 2.5;
-   //   let pointSizeL =15;
-    //  rect(x, y, pointSizeW, pointSizeL);
-   // }
-   // else {
-   //   let pointSizeW = 2;
-    //  let pointSizeL =50;
-     // rect(x, y, pointSizeL, pointSizeW); 
-          
-   // }
-    
-  //}
+ 
+  
+
 
   angleMode(DEGREES);
   let num_lines_to_draw = 40;
@@ -63,35 +57,39 @@ function draw () {
   for(let j=renderCounter; j<renderCounter+num_lines_to_draw && j<Y_STOP; j++) {
     for(let i=0; i<X_STOP; i++) {
       colorMode(RGB);
+     
       let mask = maskImg.get(i-420,j);
-       
+      
+      
+
 
       if (mask[1] < 128) {
-    
-      
         let wave = sin(i);
-
-      let slip = map(wave, -2, 2, OFFSET, OFFSET);
-      
-      pix = sourceImg.get(i+slip-420, j+slip);
-      
+        let slip = map(wave, -2, 2, OFFSET, OFFSET);
+        pix = sourceImg.get(i+slip-420, j+slip);
+        
       }
       
+
       else {
+        
         let wave = sin(j*4);
         let slip = map(wave, -3, -0, -OFFSET, OFFSET);
         pix = sourceImg.get(i+slip-420, j+slip);
 
-         // let brt = map(wave, -1, 1, 0, 255);
-        // for(let c=0; c<3; c++) {
-        //   pix[c] = brt;
-        // }
+    
+    
       }
       
+   
 
       set(i, j, pix);
+      
     }
+  
   }
+
+
 
   renderCounter = renderCounter + num_lines_to_draw;
   updatePixels();
@@ -103,7 +101,11 @@ function draw () {
     // uncomment this to save the result
     // saveArtworkImage(outputFile);
   }
-  
+
+ 
+ 
+
+
 //   }
   // renderCounter = renderCounter + 1;
   // if(renderCounter > 10) {
